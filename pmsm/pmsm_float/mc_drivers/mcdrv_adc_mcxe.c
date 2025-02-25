72ce9540a78c114815212fd58f746f413965836c
nxa18911 <peter.butko@nxp.com>
2025-02-25 12:41:02 +0100

    [MCUX-63076] Added A20 drives
    Signed-off-by: nxa18911 <peter.butko@nxp.com>

---
+++ pmsm/pmsm_float/mc_drivers/mcdrv_adc_mcxe.c
@@ -0,0 +1,242 @@
+/*
+* Copyright 2016, Freescale Semiconductor, Inc.
+* Copyright 2016-2021, 2025 NXP
+*
+* NXP Proprietary. This software is owned or controlled by NXP and may
+* only be used strictly in accordance with the applicable license terms. 
+* By expressly accepting such terms or by downloading, installing,
+* activating and/or otherwise using the software, you are agreeing that
+* you have read, and that you agree to comply with and are bound by,
+* such license terms.  If you do not agree to be bound by the applicable
+* license terms, then you may not retain, install, activate or otherwise
+* use the software.
+ */
+
+#include "mcdrv_adc_mcxe.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+
+/*!
+ * @brief Function reads and calculates 3 phase samples based on SVM sector
+ *
+ * @param this   Pointer to the current object
+ *
+ * @return none
+ */
+void MCDRV_Curr3Ph2ShGet(mcdrv_adc_mcxe_t *this)
+{
+    GMCLIB_3COOR_T_F16 sIABCtemp;    
+
+    this->ui16AdcCurrB = (MLIB_ShLSat_F16((ADC0->R[0]), 3));     // ADC0 - first conversion
+    this->ui16AdcDCBVolt   = (MLIB_ShLSat_F16((ADC0->R[1]), 3)); // ADC0 - second conversion
+  
+    this->ui16AdcCurrC   = (MLIB_ShLSat_F16((ADC1->R[0]), 3));   // ADC1 - first conversion
+    this->ui16AdcCurrA   = (MLIB_ShLSat_F16((ADC1->R[1]), 3));   // ADC1 - second conversion
+    
+    
+    switch (*this->pui16SVMSector)
+    {
+        case 2:
+        case 3:
+            sIABCtemp.f16A = (((frac16_t)(this->ui16AdcCurrA)) - this->sCurrSec23.ui16OffsetPhaA);
+            sIABCtemp.f16C = (((frac16_t)(this->ui16AdcCurrC)) - this->sCurrSec23.ui16OffsetPhaC);
+            sIABCtemp.f16B = MLIB_Neg_F16(MLIB_AddSat_F16(sIABCtemp.f16A, sIABCtemp.f16C));
+            break;
+
+        case 4:
+        case 5:
+            sIABCtemp.f16A = (((frac16_t)(this->ui16AdcCurrA)) - this->sCurrSec45.ui16OffsetPhaA);
+            sIABCtemp.f16B = (((frac16_t)(this->ui16AdcCurrB)) - this->sCurrSec45.ui16OffsetPhaB);
+            sIABCtemp.f16C = MLIB_Neg_F16(MLIB_AddSat_F16(sIABCtemp.f16A, sIABCtemp.f16B));
+            break;
+
+        case 1:
+        case 6:
+        default:
+            sIABCtemp.f16B = (((frac16_t)(this->ui16AdcCurrB)) - this->sCurrSec16.ui16OffsetPhaB);
+            sIABCtemp.f16C = (((frac16_t)(this->ui16AdcCurrC)) - this->sCurrSec16.ui16OffsetPhaC);
+            sIABCtemp.f16A = MLIB_Neg_F16(MLIB_AddSat_F16(sIABCtemp.f16B, sIABCtemp.f16C));
+            break;
+    }
+
+    /* pass measured phase currents to the main module structure */
+    this->psIABC->f16A = sIABCtemp.f16A;
+    this->psIABC->f16B = sIABCtemp.f16B;
+    this->psIABC->f16C = sIABCtemp.f16C;
+    
+    
+    *this->pf16UDcBus = (frac16_t)(this->ui16AdcDCBVolt);
+
+}
+
+/*!
+ * @brief Set initial channel assignment for phase currents & DCB voltage
+ *
+ * @param this   Pointer to the current object
+ *
+ * @return none
+ */
+void MCDRV_Curr3Ph2ShChanAssignInit(mcdrv_adc_mcxe_t *this)
+{  
+
+}
+
+/*!
+ * @brief Function set new channel assignment for next sampling based on SVM sector
+ *
+ * @param this   Pointer to the current object
+ *
+ * @return none
+ */
+void MCDRV_Curr3Ph2ShChanAssign(mcdrv_adc_mcxe_t *this)
+{
+
+}
+
+/*!
+ * @brief Function initializes phase current channel offset measurement
+ *
+ * @param this   Pointer to the current object
+ *
+ * @return none
+ */
+void MCDRV_Curr3Ph2ShCalibInit(mcdrv_adc_mcxe_t *this)
+{
+
+    /* clear offset values */
+    this->sCurrSec16.ui16OffsetPhaB = 0U;
+    this->sCurrSec16.ui16OffsetPhaC = 0U;
+    this->sCurrSec23.ui16OffsetPhaA = 0U;
+    this->sCurrSec23.ui16OffsetPhaC = 0U;
+    this->sCurrSec45.ui16OffsetPhaA = 0U;
+    this->sCurrSec45.ui16OffsetPhaB = 0U;
+
+    this->sCurrSec16.ui16CalibPhaB = 0U;
+    this->sCurrSec16.ui16CalibPhaC = 0U;
+    this->sCurrSec23.ui16CalibPhaA = 0U;
+    this->sCurrSec23.ui16CalibPhaC = 0U;
+    this->sCurrSec45.ui16CalibPhaA = 0U;
+    this->sCurrSec45.ui16CalibPhaB = 0U;
+
+    /* initialize offset filters */
+    this->sCurrSec16.ui16FiltPhaB.u16Sh = this->ui16OffsetFiltWindow;
+    this->sCurrSec16.ui16FiltPhaC.u16Sh = this->ui16OffsetFiltWindow;
+    this->sCurrSec23.ui16FiltPhaA.u16Sh = this->ui16OffsetFiltWindow;
+    this->sCurrSec23.ui16FiltPhaC.u16Sh = this->ui16OffsetFiltWindow;
+    this->sCurrSec45.ui16FiltPhaA.u16Sh = this->ui16OffsetFiltWindow;
+    this->sCurrSec45.ui16FiltPhaB.u16Sh = this->ui16OffsetFiltWindow;
+
+    GDFLIB_FilterMAInit_F16((frac16_t)0, &this->sCurrSec16.ui16FiltPhaB);
+    GDFLIB_FilterMAInit_F16((frac16_t)0, &this->sCurrSec16.ui16FiltPhaC);
+    GDFLIB_FilterMAInit_F16((frac16_t)0, &this->sCurrSec23.ui16FiltPhaA);
+    GDFLIB_FilterMAInit_F16((frac16_t)0, &this->sCurrSec23.ui16FiltPhaC);
+    GDFLIB_FilterMAInit_F16((frac16_t)0, &this->sCurrSec45.ui16FiltPhaA);
+    GDFLIB_FilterMAInit_F16((frac16_t)0, &this->sCurrSec45.ui16FiltPhaB);
+
+}
+
+/*!
+ * @brief Function reads current samples and filter them based on SVM sector
+ *
+ * @param this   Pointer to the current object
+ *
+ * @return none
+ */
+void MCDRV_Curr3Ph2ShCalib(mcdrv_adc_mcxe_t *this)
+{
+  
+    switch (*this->pui16SVMSector)
+    {
+        case 2:
+        case 3:
+
+            /* sensing of offset IA, IC*/
+
+            this->sCurrSec23.ui16CalibPhaA =
+                GDFLIB_FilterMA_F16((frac16_t)(this->ui16AdcCurrA), &this->sCurrSec23.ui16FiltPhaA);
+            this->sCurrSec23.ui16CalibPhaC =
+                GDFLIB_FilterMA_F16((frac16_t)(this->ui16AdcCurrC), &this->sCurrSec23.ui16FiltPhaC);
+            break;
+
+        case 4:
+        case 5:
+            /* sensing of offset IA, IB*/
+            this->sCurrSec45.ui16CalibPhaA =
+                GDFLIB_FilterMA_F16((frac16_t)(this->ui16AdcCurrA), &this->sCurrSec45.ui16FiltPhaA);
+            this->sCurrSec45.ui16CalibPhaB =
+                GDFLIB_FilterMA_F16((frac16_t)(this->ui16AdcCurrB), &this->sCurrSec45.ui16FiltPhaB);
+
+            break;
+
+        case 1:
+        case 6:
+
+        default:
+            /* sensing of offset IB, IC*/
+            this->sCurrSec16.ui16CalibPhaB =
+                GDFLIB_FilterMA_F16((frac16_t)(this->ui16AdcCurrB), &this->sCurrSec16.ui16FiltPhaB);
+            this->sCurrSec16.ui16CalibPhaC =
+                GDFLIB_FilterMA_F16((frac16_t)(this->ui16AdcCurrC), &this->sCurrSec16.ui16FiltPhaC);
+
+            break;
+    }
+
+}
+
+/*!
+ * @brief Function passes measured offset values to main structure
+ *
+ * @param this   Pointer to the current object
+ *
+ * @return none
+ */
+void MCDRV_Curr3Ph2ShCalibSet(mcdrv_adc_mcxe_t *this)
+{
+
+    /* pass calibration data for sector 1 and 6 */
+    this->sCurrSec16.ui16OffsetPhaB = this->sCurrSec16.ui16CalibPhaB;
+    this->sCurrSec16.ui16OffsetPhaC = this->sCurrSec16.ui16CalibPhaC;
+
+    /* pass calibration data for sector 2 and 3 */
+    this->sCurrSec23.ui16OffsetPhaA = this->sCurrSec23.ui16CalibPhaA;
+    this->sCurrSec23.ui16OffsetPhaC = this->sCurrSec23.ui16CalibPhaC;
+
+    /* pass calibration data for sector 4 and 5 */
+    this->sCurrSec45.ui16OffsetPhaA = this->sCurrSec45.ui16CalibPhaA;
+    this->sCurrSec45.ui16OffsetPhaB = this->sCurrSec45.ui16CalibPhaB;
+
+}
+
+/*!
+ * @brief Function reads and passes DCB voltage sample
+ *
+ * @param this   Pointer to the current object
+ *
+ * @return none
+ */
+void MCDRV_VoltDcBusGet(mcdrv_adc_mcxe_t *this)
+{
+
+}
+
+/*!
+ * @brief Function reads and passes auxiliary sample
+ *
+ * @param this   Pointer to the current object
+ *
+ * @return none
+ */
+void MCDRV_AuxValGet(mcdrv_adc_mcxe_t *this)
+{
+
+}
